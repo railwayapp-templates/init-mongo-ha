20e6b01e1145c9a0c356990d4ed3ea15b6d646d5
Melissa Hale <melissakayh@gmail.com>
2024-05-29 17:54:45 -0500

    Update main.py

--- exampleApps/python/main.py
+++ exampleApps/python/main.py
@@ -9,21 +9,21 @@ app = FastAPI()
 
 print(os.getenv("MONGO_URI"))
 
-# MongoDB client
+# MongoDB client (motor)
 client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
 db = client[os.getenv("DATABASE_NAME")]
 collection = db[os.getenv("COLLECTION_NAME")]
 
 def generate_random_string(length=8):
     letters = string.ascii_lowercase
-    print('generated random string')
+    print("generated random string")
     return ''.join(random.choice(letters) for i in range(length))
 
 @app.get("/")
 async def set_item():
     random_key = generate_random_string()
     random_value = generate_random_string()
-    print('sending to Mongo')
+    print("sending to Mongo")
     result = await collection.update_one(
         {"key": random_key}, {"$set": {"value": random_value}}, upsert=True
     )
@@ -35,21 +35,22 @@ async def set_item():
 @app.get("/get", response_model=List[Dict[str, str]])
 async def get_all_items():
     cursor = collection.find({})
+    print("retrieving from Mongo")
     items = await cursor.to_list(length=None)
-    # Convert ObjectId to string
     for item in items:
         item['_id'] = str(item['_id'])
     return items
 
 @app.get("/health")
-async def set_item():
-    random_key = generate_random_string()
-    random_value = generate_random_string()
-    print('sending to Mongo')
-    result = await collection.update_one(
-        {"key": "healthcheck"}, {"$set": {"value": random_value}}, upsert=True
-    )
-    print(result)
-    if result.upserted_id or result.modified_count:
-        return {"status": "success", "key": random_key, "value": random_value}
-    raise HTTPException(status_code=500, detail="Failed to set item")
+async def healthcheck():
+    print("starting healthcheck, pulling from mongo")
+    cursor = collection.aggregate([{'$sample': {'size': 1}}])
+    random_doc = await cursor.to_list(length=1)
+    
+    if random_doc:
+        print("healthcheck successful")
+        print(random_doc[0])
+        return {"status": "success", "document": random_doc[0]}
+    else:
+        print("No documents found")
+        raise HTTPException(status_code=500, detail="No documents found in the collection")
