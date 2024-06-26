c56b8346b4104d47be224126720a141e60e1eea6
Melissa Hale <melissakayh@gmail.com>
2024-06-26 14:16:32 -0500

    keyfile env var

--- nodes/generate-keyfile.sh
+++ nodes/generate-keyfile.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# Path to the keyfile and database directory
 KEYFILE_PATH="/data/keyfile"
+DB_PATH="/data/db"
 
 # Check if the keyfile already exists
 if [ -f "$KEYFILE_PATH" ]; then
   echo "Keyfile already exists at $KEYFILE_PATH. Skipping keyfile generation."
 else
-  # Generate the keyfile from the MONGO_KEYFILE_VALUE environment variable
-  if [ -z "$MONGO_KEYFILE_VALUE" ]; then
-    echo "MONGO_KEYFILE_VALUE environment variable is not set. Exiting."
+  # Generate the keyfile from the environment variable
+  if [ -z "$KEYFILE" ]; then
+    echo "KEYFILE environment variable is not set. Exiting."
     exit 1
   fi
 
   echo "Generating keyfile from environment variable..."
-  echo $MONGO_KEYFILE_VALUE > $KEYFILE_PATH
+  echo $KEYFILE > $KEYFILE_PATH
   chmod 600 $KEYFILE_PATH
+fi
+
+# Ensure the database directory exists
+if [ ! -d "$DB_PATH" ]; then
+  echo "Creating MongoDB data directory at $DB_PATH..."
+  mkdir -p $DB_PATH
+  chown -R mongodb:mongodb $DB_PATH
 fi
